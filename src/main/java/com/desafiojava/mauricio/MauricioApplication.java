ece3edd73c8cbff1c612574db848c96247e90b12
Mauricio Luan <mauricioluan2023@gmail.com>
2025-05-20 17:03:08 -0300

    primeiro commit

---
+++ src/main/java/com/desafiojava/mauricio/MauricioApplication.java
@@ -0,0 +1,13 @@
+package com.desafiojava.mauricio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauricioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauricioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiojava/mauricio/MauricioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiojava.mauricio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MauricioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
